04535dd2a58ae19cd17bd16f3bb610d330cbb444
kjw12161-wq <kjw12161@gmail.com>
2026-09-13 00:40:28 +0900

    Make exam page navigation robust

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 (() => {
   'use strict';
+
+  // The home page uses a normal <a href="exam.html"> link so navigation does not
+  // depend on JavaScript. This handler is kept as a fallback for old cached markup.
   const startBtn = document.getElementById('startExamBtn');
-  if (startBtn) {
+  if (startBtn && startBtn.tagName !== 'A') {
     startBtn.addEventListener('click', () => {
-      window.location.href = 'exam.html';
+      window.location.assign('./exam.html');
     });
   }
 })();
